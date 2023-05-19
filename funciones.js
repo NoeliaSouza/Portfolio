08cfca17ce3440543ad9e433c2924339cf2e64d2
NoeliaSouza <noesouza@outlook.es>
2023-05-19 10:00:41 -0300

    Primera subida

---
+++ funciones.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var nombre = document.getElementById('txtNombre');
+    var tel = document.getElementById('txtTel');
+    var email = document.getElementById('txtEmail');
+    var mensaje = document.getElementById('txtMensaje');
+    
+    nombre.value = '';
+    tel.value = '';
+    email.value = '';
+    mensaje.value = '';
+}
+
