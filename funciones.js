380c85e6508543c45b76b033a1ceb7b2a5222dde
NoeliaSouza <noesouza@outlook.es>
2023-05-19 15:13:45 -0300

    Validaciones

--- funciones.js
+++ funciones.js
@@ -2,11 +2,34 @@ window.onload = function() {
     var nombre = document.getElementById('txtNombre');
     var tel = document.getElementById('txtTel');
     var email = document.getElementById('txtEmail');
-    var mensaje = document.getElementById('txtMensaje');
-    
+    var mensaje = document.getElementById('txtMensaje'); 
     nombre.value = '';
     tel.value = '';
     email.value = '';
     mensaje.value = '';
 }
+
+function validarFormulario() {
+    var nombre = document.getElementById("txtNombre").value;
+    var tel = document.getElementById("txtTel").value;
+    var email = document.getElementById("txtEmail").value;
+    var mensaje = document.getElementById("txtMensaje").value;
+    if (nombre === "" || tel === "" || email === "" || mensaje === "") {
+      return false;
+    } 
+    else{
+        return true;
+    }
+}
+
+function enviarCorreo() {
+    var formularioValido = validarFormulario();
+    if (formularioValido) {
+      return true;
+    } else {
+    alert("Por favor, completá todos los campos.");
+      return false; 
+    }
+  }
+
 
